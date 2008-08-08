19e41cee945984d0510cae48838155b4a3bf0e7b
rokickik <rokickik>
2008-08-08 14:59:40 +0000

    Updated pyCaCORE egg location to use new GForge site
    SVN-Revision: 16

--- pyevs/trunk/setup.py
+++ pyevs/trunk/setup.py
@@ -18,7 +18,7 @@ setup(name='pyEVS',
       install_requires=['pyCaCORE==0.1.0'],
       namespace_packages = ['cabig'],
       dependency_links = [
-            'https://gforge.nci.nih.gov/frs/download.php/4256/pyCaCORE-0.1.0-py2.5.egg'
+            'https://gforge.nci.nih.gov/frs/download.php/4589/pyCaCORE-0.1.0-py2.5.egg'
       ],
       test_suite = 'tests.unit_tests'
      )
